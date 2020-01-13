21950e63e0f002af9a083a1f32e6bed2e9eef1a5
alexandros.synadinos@gugroup.com <alexandros.synadinos@gugroup.com>
2020-01-13 14:05:46 +0200

    fix

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var accepts = require('accepts')
 
-
+const { createEventAdapter } = require('@slack/events-api');
+const slackSigningSecret = '22172351637aea2e191b0458a0ca5edb';
+const port = process.env.PORT || 3000;
+const slackEvents = createEventAdapter(slackSigningSecret);
 
 var indexRouter = require('./routes/index');
 
 
 var app = express();
-const port = 4000;
 
 
 app.use(logger('dev'));
@@ -24,14 +26,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-app.use('/', indexRouter);
+app.use('/' , slackEvents.requestListener());
 
 
 
 
-app.listen(port, () => {
-  console.log(`Server listening at ${port}`);
+// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
+slackEvents.on('message', (event) => {
+  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
 });
+
+(async () => {
+  const server = await slackEvents.start(port);
+  console.log(`Listening for events on ${server.address().port}`);
+})();
+
+
+/*app.listen(port, () => {
+  console.log(`Server listening at ${port}`);
+});*/
 
 
 
