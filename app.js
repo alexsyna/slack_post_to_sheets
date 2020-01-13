9b3e542f122946413e48a67d057a613dc08f7787
alexandros.synadinos@gugroup.com <alexandros.synadinos@gugroup.com>
2020-01-13 14:56:34 +0200

    fix

--- app.js
+++ app.js
@@ -16,20 +16,11 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-
-
-
 app.use('/gu' , slackEvents.requestListener());
 
-
+app.use(bodyParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
 slackEvents.on('message', (event) => {
